ef9718e9917dfaa58be0b52f60c90e4d91c80002
Noire <joranmostert@outlook.com>
2023-03-24 14:23:17 +0100

    tables.sql change, added 3 new tables

---
+++ src/back-end/src/routes/users.php
@@ -0,0 +1,48 @@
+<?php
+
+use Boodschappenservice\core\Request;
+use Boodschappenservice\core\Route;
+use Boodschappenservice\structure\User;
+use Boodschappenservice\utilities\API;
+use Boodschappenservice\utilities\ResponseCode;
+use Boodschappenservice\utilities\RegExp;
+
+Route::get("/users", function(Request $request) {
+    $users = User::getAll();
+    API::printAndExit($users);
+});
+
+Route::handle(RegExp::compile("/^\/user\/(\d+)$/"), function(Request $request, array $matches) {
+    $userId = intval($matches[1][0]);
+    $user = User::get($userId);
+
+    switch($request->method) {
+        case "GET":
+            API::printAndExit($user);
+        case "DELETE":
+            $user->delete();
+            API::printAndExit([], ResponseCode::OK[0]);
+        case "PATCH":
+            $_PATCH = $request->body;
+            $user->username = !empty($_PATCH['username']) ? $_PATCH['username'] : $user->username;
+            $user->password = !empty($_PATCH['password']) ? password_hash($_PATCH['password'], PASSWORD_DEFAULT) : $user->password;
+            $user->email = !empty($_PATCH['email']) ? $_PATCH['email'] : $user->email;
+            $user->role = !empty($_PATCH['role']) ? $_PATCH['role'] : $user->role;
+            $user->save();
+            API::printAndExit($user);
+        default:
+            API::printAndExit([], ResponseCode::METHOD_NOT_ALLOWED[0]);
+    }
+});
+
+Route::post("/user", function(Request $request) {
+    try {
+        API::printAndExit(User::create(
+            $request->body['username'],
+            $request->body['password'],
+            $request->body['email'],
+            $request->body['role']));
+    } catch(\Exception $e) {
+        API::printAndExit($e->getMessage(), $e->getCode());
+    }
+});
---
+++ src/back-end/src/structure/Inkooporder.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Boodschappenservice\structure;
+
+use Boodschappenservice\utilities\ArrayList;
+
+/**
+ * @property-read int $inkoopId
+ */
+class Inkooporder implements \JsonSerializable {
+
+    /**
+     * @return Array<Inkooporder>
+     * @throws \Exception
+     */
+    public static function getAll() : array {
+        global $conn;
+        $stmt = $conn->prepare("SELECT inkoopId FROM `inkooporders`");
+        $res = $stmt->execute();
+        if($res) {
+            $inkooporders = new ArrayList();
+            $stmt->bind_result($inkoopId);
+            while($stmt->fetch()) {
+                $inkooporders->add($inkoopId);
+            }
+
+            return $inkooporders->map(function($inkoopId) {
+                return Inkooporder::get($inkoopId);
+            })->getArray();
+        } else throw new \Exception($stmt->error, 500);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function create(int $artId, int $levId, int $inkoopAantal, string $inkoopDatum) : Inkooporder {
+        global $conn;
+        $stmt = $conn->prepare("INSERT INTO `inkooporders` (artId, levId, inkoopAantal, inkoopDatum) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("iiis", $artId, $levId, $inkoopAantal, $inkoopDatum);
+        $res = $stmt->execute();
+        if($res) {
+            $inkoopId = $conn->insert_id;
+            return Inkooporder::get($inkoopId);
+        } else throw new \Exception($stmt->error, 500);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function get(int $inkoopId) : Inkooporder {
+        return new Inkooporder($inkoopId);
+    }
+
+    public int $inkoopId;
+
+    public int $artId, $levId, $inkoopAantal;
+    public string $inkoopDatum;
+
+    /**
+     * @throws \Exception
+     */
+    private function __construct(int $inkoopId) {
+        global $conn;
+        $stmt = $conn->prepare("SELECT * FROM `inkooporders` WHERE inkoopId = ?");
+        $stmt->bind_param("i", $inkoopId);
+        $res = $stmt->execute();
+        if($res) {
+            $stmt->bind_result($inkoopId, $artId, $levId, $inkoopAantal, $inkoopDatum);
+            if($stmt->fetch() === null)
+                throw new \Exception("Inkooporder with inkoopId $inkoopId does not exist", 404);
+
+            $this->inkoopId = $inkoopId;
+            $this->artId = $artId;
+            $this->levId = $levId;
+            $this->inkoopAantal = $inkoopAantal;
+            $this->inkoopDatum = $inkoopDatum;
+        } else throw new \Exception($stmt->error, 500);
+    }
+
+    public function save() {
+        global $conn;
+        $stmt = $conn->prepare("UPDATE `inkooporders` SET artId = ?, levId = ?, inkoopAantal = ?, inkoopDatum = ? WHERE inkoopId = ?");
+        $stmt->bind_param("iiisi", $this->artId, $this->levId, $this->inkoopAantal, $this->inkoopDatum, $this->inkoopId);
+        if (!$stmt->execute()) throw new \Exception($stmt->error, 500);
+    }
+
+    public function delete() {
+        global $conn;
+        $stmt = $conn->prepare("DELETE FROM `inkooporders` WHERE inkoopId = ?");
+        $stmt->bind_param("i", $this->inkoopId);
+        if (!$stmt->execute()) throw new \Exception($stmt->error, 500);
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            "inkoopId" => $this->inkoopId,
+            "artId" => $this->artId,
+            "levId" => $this->levId,
+            "inkoopAantal" => $this->inkoopAantal,
+            "inkoopDatum" => $this->inkoopDatum
+        ];
+    }
+}
+
