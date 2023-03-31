d4fbc1c6655427b682e9af5482a74b291aaa9edb
Raygell <me@rmjtromp.com>
2023-03-31 10:52:18 +0200

    Removed setup from init file

--- src/back-end/index.php
+++ src/back-end/index.php
@@ -1,6 +1,9 @@
 <?php
 
+    use Boodschappenservice\core\Request;
+    use Boodschappenservice\core\Route;
     use Boodschappenservice\utilities\API;
+    use Boodschappenservice\utilities\File;
 
     require "init.php";
 
@@ -8,5 +11,13 @@
     foreach (glob('./src/routes/*.php') as $route) {
         include $route;
     }
+
+    Route::get("/setup", function(Request $request) {
+        global $conn; // $conn is al gedefinieerd
+        require_once '../tables.php';
+        create_tables($conn);
+        insert_test_data($conn);
+        API::printAndExit("Tables initialized", 200);
+    });
 
 //    API::printAndExit([], 404);
