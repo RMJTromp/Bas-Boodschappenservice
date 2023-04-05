52e8432099d2f4545d35a8f07af77ef2bc54f9d4
RMJTromp <raygell_tromp@icloud.com>
2023-04-05 18:51:20 +0200

    Removed setup endpoint

--- src/back-end/index.php
+++ src/back-end/index.php
@@ -2,7 +2,6 @@
 
     use Boodschappenservice\core\Route;
     use Boodschappenservice\utilities\API;
-    use Boodschappenservice\utilities\File;
 
     require "init.php";
 
@@ -12,21 +11,3 @@
     }
 
     Route::get("/", fn() => API::printAndExit([]));
-
-    Route::get("/setup", function() {
-        switch ($_GET['confirm'] ?? "") {
-            case "true":
-                global $conn;
-                $file = new File("tables.sql");
-                if($conn->multi_query($file->getContents())) API::printAndExit("Tables initialized");
-                else throw new Exception("Failed to initialize tables: {$conn->error}", 500);
-            case "false":
-                API::printAndExit("Setup aborted", 499);
-            default:
-                printAndExit(<<<EOF
-                    <script>
-                        window.location.href = new URL('?confirm=' + String(confirm("Are you sure you want to initiate setup?")), window.location.href).href;
-                    </script>                
-                EOF);
-        }
-    });
