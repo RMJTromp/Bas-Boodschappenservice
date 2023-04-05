ce116347b0c5a2a82759cdb680b1b639249a5f52
RMJTromp <raygell_tromp@icloud.com>
2023-04-05 14:54:15 +0200

    reworked how `/setup` works

--- src/back-end/index.php
+++ src/back-end/index.php
@@ -12,16 +12,19 @@
     }
 
     Route::get("/setup", function() {
-        if(($_GET['confirm'] ?? "false") !== "true") {
-            printAndExit(<<<EOF
-                <script>
-                    window.location.href = new URL('?confirm=' + String(confirm("Are you sure you want to initiate setup?")), window.location.href).href;
-                </script>                
-            EOF);
+        switch ($_GET['confirm'] ?? "") {
+            case "true":
+                global $conn;
+                $file = new File("tables.sql");
+                if($conn->multi_query($file->getContents())) API::printAndExit("Tables initialized");
+                else throw new Exception("Failed to initialize tables: {$conn->error}", 500);
+            case "false":
+                API::printAndExit("Setup aborted", 499);
+            default:
+                printAndExit(<<<EOF
+                    <script>
+                        window.location.href = new URL('?confirm=' + String(confirm("Are you sure you want to initiate setup?")), window.location.href).href;
+                    </script>                
+                EOF);
         }
-
-        global $conn;
-        $file = new File("tables.sql");
-        if($conn->multi_query($file->getContents())) API::printAndExit("Tables initialized & populated");
-        else throw new Exception("Failed to initialize tables: {$conn->error}", 500);
     });
