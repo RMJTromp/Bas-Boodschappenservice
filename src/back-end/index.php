0890500141552abbeca30b96ad2374dc33df8c93
RMJTromp <raygell_tromp@icloud.com>
2023-04-07 01:01:48 +0200

    Added front-end

--- src/back-end/index.php
+++ src/back-end/index.php
@@ -1,13 +1,24 @@
 <?php
 
+    use Boodschappenservice\core\Request;
     use Boodschappenservice\core\Route;
     use Boodschappenservice\utilities\API;
+    use Boodschappenservice\utilities\File;
 
     require "init.php";
 
-    // load all routes
-    foreach (glob('./src/routes/*.php') as $route) {
-        include $route;
-    }
+    global $REQUEST_URL;
 
-    Route::get("/", fn() => API::printAndExit([]));
+    if($REQUEST_URL->hostname === "boodschappenservice.loc") {
+        Route::$handled = true;
+        Route::$methods = [ Request::get()->method ];
+
+        printAndExit(new File("../front-end/dist/index.html"));
+    } else {
+        Route::get("/", fn() => API::printAndExit(null));
+
+        // load all routes
+        foreach (glob('./src/routes/*.php') as $route) {
+            include $route;
+        }
+    }
