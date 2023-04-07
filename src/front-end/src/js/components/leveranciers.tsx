2c6f540484ca83327e6496083c4aa41e3a24a8a8
RMJTromp <raygell_tromp@icloud.com>
2023-04-07 14:19:55 +0200

    start front-end

---
+++ src/front-end/src/js/components/leveranciers.tsx
@@ -0,0 +1,12 @@
+import { h } from "dom-chef";
+
+export function Leveranciers() {
+    return (
+        <div className="section" id="leveranciers">
+            <div className="container">
+                <h1>Leveranciers</h1>
+                <p>Deze pagina is nog in ontwikkeling.</p>
+            </div>
+        </div>
+    )
+}
--- src/front-end/src/js/script.tsx
+++ src/front-end/src/js/script.tsx
@@ -1,6 +1,46 @@
 import { h } from "dom-chef";
-import TestComponent from "./components/TestComponent";
 
-document.body.append(<TestComponent username={"joran"} />);
+function build() {
+    document.body.innerHTML = "";
+    const url = new URL(window.location.href);
+    if(url.pathname === "/leveranciers") {
 
-fetch("http://api.boodschappenservice.loc/leveranciers").then(res => res.json).then(console.log);
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    // document.body.append(
+    //     <Titlebar/>,
+    //     <main>
+    //         <section id="nav">
+    //             <div class="container">
+    //                 <Navbar/>
+    //             </div>
+    //         </section>
+    //     </main>,
+    //     <SearchBar/>
+    // );
+
+    window.history.pushState = new Proxy(window.history.pushState, {
+        apply: async (target, thisArg, argArray) => {
+            target.apply(thisArg, argArray);
+            build();
+        },
+    });
+
+    window.onclick = (e) => {
+        if(e.target.matches("a[href]")) {
+            if(e.target.host === window.location.host) {
+                e.preventDefault();
+
+                if(e.target.href !== window.location.href)
+                    window.history.pushState({}, null, e.target.href);
+            }
+        }
+    }
+
+    window.onpopstate = () => build();
+
+    build();
+});
